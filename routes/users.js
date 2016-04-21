a3cb481c4b5a858f7dbc72605b5b7673f8346129
Kluminati <kluminati11@gmail.com>
2016-04-21 15:57:44 +0300

    'Yes'

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/gg', function(req, res, next) {
+  res.send('<h1>Node Js Server Runing</h1>');
+});
 module.exports = router;
