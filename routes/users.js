8b44ea6765ef5babcc115f15d73ed23da317c0b1
Kluminati <kluminati11@gmail.com>
2016-04-21 15:59:34 +0300

    'Yes'

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1>users</h1>');
 });
 
-router.get('/gg', function(req, res, next) {
-  res.send('<h1>Node Js Server Runing</h1>');
-});
 module.exports = router;
