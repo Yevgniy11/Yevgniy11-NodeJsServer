e32f334c11832be902d55f2741e5dc6989a8633a
Kluminati <kluminati11@gmail.com>
2016-04-21 16:24:19 +0300

    'Yes'

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var snippetdbutils = require('./routes/snippetdbutils');
-var fdd = require('./');
+
 
 var app = express();
 
@@ -27,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/snippetdbutils', snippetdbutils);
-app.use('/fdd', fdd);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
