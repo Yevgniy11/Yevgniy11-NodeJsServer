6a99785b1c83314f891723085b4c0b712ffc74f0
Kluminati <kluminati11@gmail.com>
2016-04-21 20:56:22 +0300

    'Yes'

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.send('<h1>users</h1>');
 });
 
-router.post('/initSnippetObjectDb', (req, res)=>{
+router.get('/initSnippetObjectDb', (req, res)=>{
   pg.connect(process.env.DATABASE_URL, (err, client, done)=>{
     var query =  'CREATE TABLE SnippetObject(id SERIAL PRIMARY KEY  , title VARCHAR ,likes INT ,comments VARCHAR,user VARCHAR,input VARCHAR);';
     client.query(query, (err, result)=>{
@@ -25,7 +25,7 @@ router.get('/drop', (req, res)=>{
     var query =  'DROP TABLE SnippetObject';
     client.query(query, (err, result)=>{
       if(!err)
-      res.json({'success':true, "message":"talbes created",'result':result});
+      res.json({'success':true, "message":"talbes droped",'result':result});
       else {
         res.json({'success':false, "message":"Table is not droped",'error':err});
       }
