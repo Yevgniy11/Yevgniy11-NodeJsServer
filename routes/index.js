32ceada72c8c92c864c1d6dfb85497232ccc5b19
Kluminati <kluminati11@gmail.com>
2016-04-25 23:05:44 +0300

    File Upload

--- routes/index.js
+++ routes/index.js
@@ -278,7 +278,7 @@ router.post('/select', (req, res)=>{
           var newCommentValue = JSON.stringify(com);
           res.json({'success':com,'rr':newCommentValue});
           var updateQuery = "UPDATE SnippetObject SET comments =$1 WHERE id=$2 ;"
-          client.query(updateQuery,[com,id], (err, result)=>{
+          client.query(updateQuery,[newCommentValue,id], (err, result)=>{
             if(!err){
               res.json({'success':"true", "message":"Update is successful",'result':result});
             }
