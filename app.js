e78dccd75e214941a0e590ffb014a22dd0f92f5e
Kluminati <kluminati11@gmail.com>
2016-04-28 16:01:20 +0300

    aasd

--- app.js
+++ app.js
@@ -24,11 +24,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static('public/images'));
 
 
 app.use('/', routes);
 app.use('/users', users);
+
 //app.use('./uploads',uploads);
 //app.use('/tmp', tmp);
 //app.use('/snippetdbutils', snippetdbutils);
