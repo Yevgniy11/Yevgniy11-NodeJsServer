87178cb31c9aab6e7d843249b037f81848048f69
Kluminati <kluminati11@gmail.com>
2016-04-28 14:04:01 +0300

    da

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 //var uploads = require('./uploads');
 //var tmp = require('./tmp');
-//var snippetdbutils = require('./routes/snippetdbutils');
+var snippetdbutils = require('./routes/snippetdbutils');
 
 
 var app = express();
@@ -30,7 +30,7 @@ app.use('/', routes);
 app.use('/users', users);
 //app.use('./uploads',uploads);
 //app.use('/tmp', tmp);
-//app.use('/snippetdbutils', snippetdbutils);
+app.use('/snippetdbutils', snippetdbutils);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
